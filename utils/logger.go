822eb2a6ece310451ab787f98f53960aa5cce366
Jubril Oyetunji <cyberdev01@protonmail.com>
2021-06-12 23:05:43 +0100

    adds new logger

--- utils/logger.go
+++ utils/logger.go
@@ -3,10 +3,19 @@ package utils
 import (
 	"bytes"
 	"log"
+
+	"github.com/fatih/color"
 )
 
 func NewDonutLogger() *log.Logger {
 	var buf bytes.Buffer
 	LG := log.New(&buf, "[Donut-Server]", log.Lshortfile)
+	return LG
+}
+
+func NewDBLogger() *log.Logger {
+	prefix := color.YellowString("[DATABASE]")
+	var buf bytes.Buffer
+	LG := log.New(&buf, prefix, log.Lshortfile)
 	return LG
 }
