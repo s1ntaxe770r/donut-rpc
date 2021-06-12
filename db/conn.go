e31f2b1b3843f8e6b9a38f1ef6f20c538870ff61
Jubril Oyetunji <cyberdev01@protonmail.com>
2021-06-12 23:06:34 +0100

    add new methods

---
+++ db/conn.go
@@ -0,0 +1,43 @@
+package db
+
+import (
+	"github.com/fatih/color"
+	donut_rpc "github.com/s1ntaxe770r/donut-rpc/proto"
+	"github.com/s1ntaxe770r/donut-rpc/utils"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var (
+	dblg = utils.NewDBLogger()
+)
+
+// Connect instantiates a new database client
+func Connect() *gorm.DB {
+	db, err := gorm.Open(sqlite.Open("inventory.db"), &gorm.Config{})
+	if err != nil {
+		dblg.Panicln("UNABLE TO CONNECT TO DATABASE")
+	}
+	db.AutoMigrate(&donut_rpc.Donut{})
+	dblg.Println(color.GreenString("DB CONNECTION ESTABLISHED"))
+	return db
+}
+
+func MakeDonut(db *gorm.DB, donut *donut_rpc.Donut) (*donut_rpc.Donut, error) {
+	bakerr := db.Create(donut).Error
+	if bakerr != nil {
+		dblg.Fatal(bakerr.Error())
+		return donut, bakerr
+	}
+	return donut, nil
+}
+
+func GetDonut(db *gorm.DB, donut *donut_rpc.DonutRequest) (*donut_rpc.Donut, error) {
+	var res donut_rpc.Donut
+	err := db.First(&res, donut.Name).Error
+	if err != nil {
+		dblg.Fatal(err.Error())
+		return &res, err
+	}
+	return &res, nil
+}
