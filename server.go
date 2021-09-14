5243bc1c026ebebf699546b7ffc7bd02a337716e
Jubril Oyetunji <cyberdev01@protonmail.com>
2021-09-14 15:08:00 +0100

    collect grpc metrics with prometheus

--- server.go
+++ server.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net"
+	"net/http"
 
+	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/s1ntaxe770r/donut-rpc/db"
 	pb "github.com/s1ntaxe770r/donut-rpc/proto"
 	"github.com/s1ntaxe770r/donut-rpc/utils"
@@ -17,9 +22,28 @@ const (
 )
 
 var (
-	lg   = utils.NewDonutLogger()
-	conn = db.Connect()
+	lg              = utils.NewDonutLogger()
+	conn            = db.Connect()
+	rpcMetrics      = grpc_prometheus.NewServerMetrics()
+	reg             = prometheus.NewRegistry()
+	GetDonutCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "get_donut_request_counter",
+		Help: "number of requests served by the get donut method",
+	}, []string{"getDonut"})
+	MakeDonutCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "make_donut_request_counter",
+		Help: "number of requests served by the make donut method",
+	}, []string{"MakeDonut"})
+	GetDonutsCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "get_donust_request_counter",
+		Help: "number of requests served by the get donuts method",
+	}, []string{"getDonuts"})
 )
+
+func init() {
+	reg.MustRegister(rpcMetrics, GetDonutCounter, GetDonutsCounter, MakeDonutCounter)
+
+}
 
 type DonutServer struct {
 	pb.UnimplementedDonutShopServer
@@ -28,6 +52,7 @@ type DonutServer struct {
 
 func (ds *DonutServer) GetDonut(ctx context.Context, in *pb.DonutRequest) (*pb.Donut, error) {
 	donut, err := db.GetDonut(conn, in)
+	GetDonutCounter.WithLabelValues(in.Name).Inc()
 	ds.lg.Println(in)
 	if err != nil {
 		return nil, err
@@ -37,6 +62,7 @@ func (ds *DonutServer) GetDonut(ctx context.Context, in *pb.DonutRequest) (*pb.D
 
 func (ds *DonutServer) GetDonuts(ctx context.Context, in *pb.DonutRequest) (*pb.Donuts, error) {
 	donuts, err := db.GetDonuts(conn)
+	GetDonutsCounter.WithLabelValues(in.Name).Inc()
 	if err != nil {
 		return nil, err
 	}
@@ -45,6 +71,7 @@ func (ds *DonutServer) GetDonuts(ctx context.Context, in *pb.DonutRequest) (*pb.
 }
 func (ds *DonutServer) MakeDonut(ctx context.Context, in *pb.Donut) (*pb.DonutRequest, error) {
 	_, err := db.MakeDonut(conn, in)
+	MakeDonutCounter.WithLabelValues(in.Name).Inc()
 	if err != nil {
 
 		return nil, err
@@ -59,11 +86,21 @@ func main() {
 		lg.Panicf("could not open shop %s", err.Error())
 	}
 
-	server := grpc.NewServer()
+	server := grpc.NewServer(
+		grpc.StreamInterceptor(rpcMetrics.StreamServerInterceptor()),
+		grpc.UnaryInterceptor(rpcMetrics.UnaryServerInterceptor()),
+	)
+	httpServer := &http.Server{Handler: promhttp.HandlerFor(reg, promhttp.HandlerOpts{}), Addr: fmt.Sprintf("0.0.0.0:%d", 9092)}
 	reflection.Register(server)
 	pb.RegisterDonutShopServer(server, &DonutServer{lg: lg})
+	rpcMetrics.InitializeMetrics(server)
 	lg.Printf("shop opened on %v", lis.Addr())
 
+	go func() {
+		if err := httpServer.ListenAndServe(); err != nil {
+			log.Fatal("Unable to start a http server.")
+		}
+	}()
 	err = server.Serve(lis)
 	if err != nil {
 		lg.Panicf("oops couldn't open up shop %s", err.Error())
